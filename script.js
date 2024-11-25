ef65dfc5b3aff52840401e842c4d428cbe838a63
Lakshmi Akshaya T <akshayat1803@gmail.com>
2024-11-25 13:52:29 +0530

    third commit

--- script.js
+++ script.js
@@ -16,20 +16,24 @@
 
        data.forEach((ele)=>{
         const shortendTitle=ele.title.slice(0,20)+"..."
-        const shortendDescription=ele.description.slice(0,50)+"..."
-        let card = `<div class="card me-4 mb-4 h-75" >
-    <img src=${ele.image} class="card-img-top w-100 h-50 p-2" alt="...">
+        const shortendDescription=ele.description.slice(0,55)+"..."
+        let card = `
+        <div class="col col-lg-4 col-md-6 col-12">
+        <div class="card " >
+    <img src=${ele.image} class="card-img-top p-2" alt="...">
   <div class="card-body">
     <h5 class="card-title">${shortendTitle}</h5>
     <p class="card-text">${shortendDescription}</p>
     </div>
-    <ul class="list-group list-group-flush">
-    <li class="list-group-item">${ele.price}</li>
+    <ul class="list-group list-group-flush text-center">
+    <li class="list-group-item fs-5 fw-lighter">$${ele.price.toFixed(2)}</li>
   </ul>
-  <div class="card-body">
+  <div class="card-body text-center">
     <a href="#" class="btn btn-dark">Details</a>
     <a href="#" class="btn btn-dark">Add to Cart</a>
   </div>
+  </div>
+
 </div>`
 
 
