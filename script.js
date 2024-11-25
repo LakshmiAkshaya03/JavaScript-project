a002e61c59bd82b40f98728afec7a79185906105
Lakshmi Akshaya T <akshayat1803@gmail.com>
2024-11-25 21:32:30 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
-    function fetchHandler(){
-        const fetchapi=fetch(' https://fakestoreapi.com/products').then((resp)=>{
-            return resp.json()
-        }).then((data)=>{
-            dataHandler(data)
-        })
-    }
-    fetchHandler()
+function fetchHandler() {
+  const fetchapi = fetch(' https://fakestoreapi.com/products').then((resp) => {
+    return resp.json()
+  }).then((data) => {
+    dataHandler(data)
+  })
+}
+fetchHandler()
 
 
-    function dataHandler(data){
-        console.log(data);
-        
-       const parentDiv = document.querySelector(".product-item-container")
+function dataHandler(data) {
 
-       data.forEach((ele)=>{
-        const shortendTitle=ele.title.slice(0,20)+"..."
-        const shortendDescription=ele.description.slice(0,55)+"..."
-        let card = `
+  const parentDiv = document.querySelector(".product-item-container")
+
+  data.forEach((ele) => {
+    const shortendTitle = ele.title.slice(0, 20) + "..."
+    const shortendDescription = ele.description.slice(0, 60) + "..."
+    let card = `
         <div class="col col-lg-4 col-md-6 col-12">
         <div class="card " >
     <img src=${ele.image} class="card-img-top p-2" alt="...">
@@ -29,7 +28,7 @@
     <li class="list-group-item fs-5 fw-lighter">$${ele.price.toFixed(2)}</li>
   </ul>
   <div class="card-body text-center">
-    <a href="#" class="btn btn-dark">Details</a>
+    <a href="details-page.html?id=${ele.id}" class="btn btn-dark ">Details</a>
     <a href="#" class="btn btn-dark">Add to Cart</a>
   </div>
   </div>
@@ -37,9 +36,9 @@
 </div>`
 
 
-parentDiv.insertAdjacentHTML("beforeend", card)
-       })
-    }
+    parentDiv.insertAdjacentHTML("beforeend", card)
+  })
+}
 
 
 
