8de9788c36dd1905694ba33cb68b0fccf5a5a3f7
Lakshmi Akshaya T <akshayat1803@gmail.com>
2024-11-27 20:10:41 +0530

    tenth commit

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
 
-function fetchHandler() {
-  const fetchapi = fetch(' https://fakestoreapi.com/products').then((resp) => {
-    return resp.json()
-  }).then((data) => {
-    dataHandler(data)
-  })
+const productContainer = document.querySelector(".products");
+let allProducts = [];
+
+function fetchProducts() {
+    fetch("https://fakestoreapi.com/products")
+        .then((resp) => resp.json())
+        .then((data) => {
+            allProducts = data; 
+            displayProducts(allProducts);
+            setupCategoryFilters() 
+        })
+        .catch((err) => console.log("Error occurred while fetching data", err));
 }
-fetchHandler()
 
+function displayProducts(filteredProducts) {
+    productContainer.innerHTML = ""; 
+    filteredProducts.forEach((product) => {
+        const reducedTitle = product.title.slice(0, 20) + "...";
+        const reducedDescription = product.description.slice(0, 60) + "...";
 
-function dataHandler(data) {
-
-  const parentDiv = document.querySelector(".product-item-container")
-
-  data.forEach((ele) => {
-    const shortendTitle = ele.title.slice(0, 20) + "..."
-    const shortendDescription = ele.description.slice(0, 60) + "..."
-    let card = `
+        const productCard = `
         <div class="col col-lg-4 col-md-6 col-12">
-        <div class="card " >
-    <img src=${ele.image} class="card-img-top p-2" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${shortendTitle}</h5>
-    <p class="card-text">${shortendDescription}</p>
-    </div>
-    <ul class="list-group list-group-flush text-center">
-    <li class="list-group-item fs-5 fw-lighter">$${ele.price.toFixed(2)}</li>
-  </ul>
-  <div class="card-body text-center">
-    <a href="details-page.html?id=${ele.id}" class="btn btn-dark ">Details</a>
-    <a href="#" class="btn btn-dark">Add to Cart</a>
-  </div>
-  </div>
-
-</div>`
-
-
-    parentDiv.insertAdjacentHTML("beforeend", card)
-  })
+            <div class="card">
+                <img src=${product.image} class="card-img-top p-2" alt="${product.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${reducedTitle}</h5>
+                    <p class="card-text">${reducedDescription}</p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item fs-5 fw-lighter">$${product.price.toFixed(2)}</li>
+                </ul>
+                <div class="card-body text-center">
+                    <a href="details-page.html?id=${product.id}" class="btn btn-dark">Details</a>
+                    <a href="#" class="btn btn-dark">Add to Cart</a>
+                </div>
+            </div>
+        </div>`;
+        productContainer.insertAdjacentHTML("beforeend", productCard);
+    });
 }
 
+
+function setupCategoryFilters() {
+    document.querySelectorAll("button[data-category]").forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault(); 
+            const categoryOfProduct = button.getAttribute("data-category");
+
+            const filteredProducts =
+                categoryOfProduct === "all"
+                    ? allProducts 
+                    : allProducts.filter((product) =>
+                          product.category.toLowerCase() === categoryOfProduct.toLowerCase()
+                      );
+
+            displayProducts(filteredProducts); 
+        });
+    });
+}
+
+fetchProducts();
 
 
